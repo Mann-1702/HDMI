1beeca0d31f8556bd5c66b50a8e3709323678a81
Mann-1702 <mannshah1702@gmail.com>
2024-10-24 13:45:42 -0700

    Added Matches.cshtml.cs file in src\Pages folder.

---
+++ src/Pages/Matches.cshtml.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Hosting;
+
+public class MatchesService
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public MatchesService(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public IEnumerable<Match> GetMatches()
+    {
+        var jsonFilePath = Path.Combine(_webHostEnvironment.WebRootPath, "data", "products.json");
+        using (var jsonFileReader = File.OpenText(jsonFilePath))
+        {
+            return JsonSerializer.Deserialize<Match[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
