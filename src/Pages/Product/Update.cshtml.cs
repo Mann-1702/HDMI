f9dd498523b13147a828694976957dfe122a11fd
Ian Tjok <ianptjok@hotmail.com>
2024-10-27 15:35:32 -0700

    Added UpdateModel class by adding Update.cshtml.cs file

---
+++ src/Pages/Product/Update.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class UpdateModel : PageModel
+    {
+        private readonly JsonFileProductService _productService;
+
+        public UpdateModel(JsonFileProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ProductModel Product { get; set; }
+
+        public IActionResult OnGet(string id)
+        {
+            Product = _productService.GetAllData().FirstOrDefault(p => p.Id == id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+    }
+}
