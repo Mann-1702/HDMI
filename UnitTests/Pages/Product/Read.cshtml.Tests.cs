71fb28d0734f0d483530b0e55f53902626157bcf
1HongyeXiong <hongye.xiong@outlook.com>
2024-10-26 22:47:27 -0700

    Added Read.cshtml.Tests.cs file in src\UnitTests\Pages\Product folder.

---
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Pages.Product.Read
+
+{
+    public class ReadTests
+    {
+
+    }
+}
