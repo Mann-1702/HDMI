9bdf475924f6f342da91b8fc94412954385b4345
Mann-1702 <mannshah1702@gmail.com>
2024-10-27 16:56:46 -0700

    Updated MatchController.cs file in src\Controllers folder.

---
+++ src/Controllers/MatchController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MatchController : ControllerBase
+    {
+        public MatchController(JsonFileMatchService matchService)
+        {
+            MatchService = matchService;
+        }
+
+        public JsonFileMatchService MatchService { get; }
+
+        [HttpGet]
+        public IEnumerable<MatchModel> Get()
+        {
+            return MatchService.GetAllData();
+        }
+    }
+
+}
