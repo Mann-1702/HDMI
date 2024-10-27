7f7b50a3737b295156dc07dc01c4c2b1031dedf9
1HongyeXiong <hongye.xiong@outlook.com>
2024-10-27 00:39:46 -0700

    Add Match model with properties for Match name, Date, and Location in Models folder

---
+++ src/Models/MatchModel.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class MatchModel
+    {
+        [Required]
+        public string Match { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "The Location should have a length of more than {2} and less than {1}")]
+        public string Location { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<MatchModel>(this);
+    }
+}
