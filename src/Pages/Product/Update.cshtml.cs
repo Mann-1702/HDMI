0178008dfbd90df01cd8e750be184b164dcf8d06
Mann-1702 <mannshah1702@gmail.com>
2024-10-27 16:45:07 -0700

    Updated Update.cshtml.cs file in src\Pages\Product folder.

--- src/Pages/Product/Update.cshtml.cs
+++ src/Pages/Product/Update.cshtml.cs
@@ -10,34 +10,35 @@ namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class UpdateModel : PageModel
     {
-    // Data #middletier
-    public JsonFileProductService ProductService { get; }
+        // Data #middletier
+        public JsonFileProductService ProductService { get; }
 
-    /// <summary> Default Constructor </summary>
+        /// <summary> Default Constructor </summary>
         public UpdateModel(JsonFileProductService productService)
         {
-        ProductService = productService;
+            ProductService = productService;
         }
 
-    // The data to show, bind to it for the post
-    [BindProperty]
+        // The data to show, bind to it for the post
+        [BindProperty]
         public ProductModel Product { get; set; }
 
-    /// <summary> REST Get request Loads the Data </summary>
-    public void OnGet(string id)
-            {
-        Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
-            }
-
-    /// <summary> Post the model back to the page. The model is in the class variable. </summary>
-    public IActionResult OnPost()
-    {
-        if (!ModelState.IsValid)
+        /// <summary> REST Get request Loads the Data </summary>
+        public void OnGet(string id)
         {
-            return Page();
+            Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
         }
 
-        ProductService.UpdateData(Product);
-        return RedirectToPage("./Index");
+        /// <summary> Post the model back to the page. The model is in the class variable. </summary>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ProductService.UpdateData(Product);
+            return RedirectToPage("./Index");
+        }
     }
 }
