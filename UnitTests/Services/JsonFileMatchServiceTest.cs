9b050d338e03f7521f8fbc5f34f0658f9a9a6294
Ian Tjok <ianptjok@hotmail.com>
2024-10-27 16:31:26 -0700

    Created unit testing file: JsonFileMatchServiceTest.cs to Unit Test for JsonFileMatchService.cs

---
+++ UnitTests/Services/JsonFileMatchServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Models;
+using System;
+using System.Threading;
+
+namespace UnitTests.Services
+{
+    public class JsonFileMatchServiceTests
+    {
+        #region TestSetup
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        #region GetAllData
+        [Test]
+        public void GetAllData_Should_Return_2_Items()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.MatchService.GetAllData();
+            int count = result.Count();
+
+            // Assert
+            Assert.That(count, Is.EqualTo(2));
+        }
+        #endregion GetAllData
+
+
+
+
+    }
+}
