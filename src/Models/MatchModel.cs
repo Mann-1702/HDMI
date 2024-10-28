442448f9e841a8688aefc7b4cfae5ea26d578474
Ian Tjok <ianptjok@hotmail.com>
2024-10-27 20:03:19 -0700

    Added 4 more fields (Team1, Team2, Team1_Score, Team2_Score)  to the MatchModel class in MatchModel.cs

--- src/Models/MatchModel.cs
+++ src/Models/MatchModel.cs
@@ -18,6 +18,15 @@ namespace ContosoCrafts.WebSite.Models
         [StringLength(100, MinimumLength = 3, ErrorMessage = "The Location should have a length of more than {2} and less than {1}")]
         public string Location { get; set; }
 
+        public string Team1 { get; set; }
+
+        public string Team2 { get; set; }
+
+        public int Team1_Score { get; set; }
+
+        public int Team2_Score { get; set; }
+
+
         public override string ToString() => JsonSerializer.Serialize<MatchModel>(this);
     }
 }
