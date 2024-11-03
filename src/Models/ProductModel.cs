b773a4f00053018ea5af3f26641ca0a1f6663650
1HongyeXiong <hongye.xiong@outlook.com>
2024-11-02 23:46:20 -0700

    Added validation logic in  ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -11,32 +11,34 @@ namespace ContosoCrafts.WebSite.Models
         public string Maker { get; set; }
 
         [JsonPropertyName("img")]
+        [Url(ErrorMessage = "Please enter a valid image URL.")]
         public string Image { get; set; }
 
+        [Url(ErrorMessage = "Please enter a valid URL.")]
         public string Url { get; set; }
-        
-        [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
         public string Description { get; set; }
 
         public int[] Ratings { get; set; }
 
         public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
-        
+
         public string Sport { get; set; }
 
         [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int FoundingYear { get; set; }
 
-        [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Trophies { get; set; }
 
         // Store the Comments entered by the users on this product
         public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
-
-
     }
 }
