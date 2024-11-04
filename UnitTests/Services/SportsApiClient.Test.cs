0e40898726fc9853547895cf33f2986c605b713d
David Waiganjo <dwaiganjo@seattleu.edu>
2024-11-03 20:50:09 -0800

    Added a SportsAPIClient.Test.cs file to bu used to test SportsAPiClient.cs

---
+++ UnitTests/Services/SportsApiClient.Test.cs
@@ -0,0 +1,16 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.Tests.Services
+{
+    public class SportsApiClientTests
+    {
+    }
+}
