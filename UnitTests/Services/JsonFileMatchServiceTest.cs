9c5e3a219084e06200e6b45164c4c57b4d028787
Ian Tjok <ianptjok@hotmail.com>
2024-11-05 22:31:01 -0800

    Re-added unit tests in JsonFileMatchServiceTest.cs

--- UnitTests/Services/JsonFileMatchServiceTest.cs
+++ UnitTests/Services/JsonFileMatchServiceTest.cs
@@ -7,8 +7,7 @@ using NUnit.Framework;
 using ContosoCrafts.WebSite.Models;
 using System;
 using System.Threading;
-
-
+using System.ComponentModel.DataAnnotations;
 
 namespace UnitTests.Services
 {
@@ -39,7 +38,208 @@ namespace UnitTests.Services
         #endregion GetAllData
 
 
+        #region SwapTeam1Team2
+        [Test]
+        public void SwapTeam1Team2_Invalid_Null_Input_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.MatchService.SwapTeam1Team2(null);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void SwapTeam1Team2_Invalid_Empty_Match_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+
+            // Act
+            var result = TestHelper.MatchService.SwapTeam1Team2(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void SwapTeam1Team2_Invalid_Team1_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+
+            // Invalid testMatch.team1 - unassigned
+            testMatch.Team2 = "Team2";
+
+            // Act
+            var result = TestHelper.MatchService.SwapTeam1Team2(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void SwapTeam1Team2_Invalid_Team2_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+
+            // Invalid testMatch.team2 - unassigned
+            testMatch.Team1 = "Team1";
+
+            // Act
+            var result = TestHelper.MatchService.SwapTeam1Team2(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void SwapTeam1Team2_Invalid_Negative_Team1_Score_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+            testMatch.Team1 = "Team1";
+            testMatch.Team2 = "Team2";
+            testMatch.Team1_Score = -1;
+
+            // Act
+            var result = TestHelper.MatchService.SwapTeam1Team2(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void SwapTeam1Team2_Invalid_Negative_Team2_Score_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+            testMatch.Team1 = "Team1";
+            testMatch.Team2 = "Team2";
+            testMatch.Team2_Score = -1;
+
+            // Act
+            var result = TestHelper.MatchService.SwapTeam1Team2(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void SwapTeam1Team2_Valid_Match_Should_Return_True()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+            testMatch.Team1 = "Team1";
+            testMatch.Team2 = "Team2";
 
 
+            // Act
+            var result = TestHelper.MatchService.SwapTeam1Team2(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(true));
+            Assert.That(testMatch.Team1, Is.EqualTo("Team2"));
+            Assert.That(testMatch.Team2, Is.EqualTo("Team1"));
+        }
+        #endregion SwapTeam1Team2
+
+        #region IsValidMatch
+        [Test]
+        public void IsValidMatch_Invalid_Null_Input_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.MatchService.IsValidMatch(null);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsValidMatch_Invalid_Null_Team1_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+            testMatch.Team1 = null;
+            testMatch.Team2 = "Team2";
+
+            // Act
+            var result = TestHelper.MatchService.IsValidMatch(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsValidMatch_Invalid_Null_Team2_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+            testMatch.Team1 = "Team2";
+            testMatch.Team2 = null;
+
+            // Act
+            var result = TestHelper.MatchService.IsValidMatch(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsValidMatch_Invalid_Negative_Team1_Score_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+            testMatch.Team1 = "Team1";
+            testMatch.Team2 = "Team2";
+            testMatch.Team1_Score = -1;
+            testMatch.Team2_Score = 0;
+
+            // Act
+            var result = TestHelper.MatchService.IsValidMatch(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsValidMatch_Invalid_Negative_Team2_Score_Should_Return_False()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+            testMatch.Team1 = "Team1";
+            testMatch.Team2 = "Team2";
+            testMatch.Team1_Score = 0;
+            testMatch.Team2_Score = -1;
+
+            // Act
+            var result = TestHelper.MatchService.IsValidMatch(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsValidMatch_Valid_Match_Should_Return_True()
+        {
+            // Arrange
+            MatchModel testMatch = new MatchModel();
+            testMatch.Team1 = "Team1";
+            testMatch.Team2 = "Team2";
+            testMatch.Team1_Score = 1;
+            testMatch.Team2_Score = 2;
+
+            // Act
+            var result = TestHelper.MatchService.IsValidMatch(testMatch);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(true));
+        }
+        #endregion IsValidMatch
     }
 }
