21776ad80f8e32fff1c8490e07b411671bbbf5b4
Ian Tjok <ianptjok@hotmail.com>
2024-11-05 22:57:54 -0800

    Changed spacing for readability on Delete.cshtml.cs

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -8,29 +8,39 @@ namespace ContosoCrafts.WebSite.Pages.Product
     public class DeleteModel : PageModel
     {
         private readonly JsonFileProductService _productService;
+
         public DeleteModel(JsonFileProductService productService)
         {
             _productService = productService;
         }
+
+
         [BindProperty]
         public ProductModel Product { get; set; }
+
+
         // Fetches the product details based on ID for confirmation page
         public IActionResult OnGet(string id)
         {
             // Attempt to find the product by the given ID
             Product = _productService.GetAllData().FirstOrDefault(p => p.Id == id);
+
             // If product not found, redirect to Index
             if (Product == null)
             {
                 return RedirectToPage("Index");
             }
+
             return Page();
         }
+
+
         // Handles the deletion upon form submission
         public IActionResult OnPost()
         {
             // Retrieve the product from the service based on the ID
             var productToDelete = _productService.GetAllData().FirstOrDefault(p => p.Id == Product.Id);
+
             if (productToDelete == null)
             {
                 return RedirectToPage("Index");
