ad2aaeec1f3b42b9310d90f2500e2fe99f70f751
David Waiganjo <dwaiganjo@seattleu.edu>
2024-11-09 22:16:19 -0800

    Added more field for NBAscores details Updates made in NBAAPIModels.cs

--- src/Models/NBAAPIModel.cs
+++ src/Models/NBAAPIModel.cs
@@ -1,60 +1,64 @@
-﻿
-    public class NbaGameResponse
-    {
-        public int Id { get; set; }
-        public string League { get; set; }
-        public int Season { get; set; }
-        public NbaGameDate Date { get; set; }
-        public string Stage { get; set; }
-        public NbaStatus Status { get; set; }
-        public NbaTeams Teams { get; set; }
-        public NbaScores Scores { get; set; }
-    }
+﻿using System.Collections.Generic;
 
-    public class NbaGameDate
-    {
-        public string Start { get; set; }    
-        public string End { get; set; }      
-        public string Duration { get; set; } 
-    }
+public class NbaGameResponse
+{
+    public int Id { get; set; }
+    public string League { get; set; }
+    public int Season { get; set; }
+    public NbaGameDate Date { get; set; }
+    public string Stage { get; set; }
+    public NbaStatus Status { get; set; }
+    public NbaTeams Teams { get; set; }
+    public NbaScores Scores { get; set; }
+}
 
-    public class NbaStatus
-    {
-        public string Short { get; set; }  
-        public string Long { get; set; }   
-        public string Clock { get; set; }  
-        public bool Halftime { get; set; } 
-    }
+public class NbaGameDate
+{
+    public string Start { get; set; }
+    public string End { get; set; }
+    public string Duration { get; set; }
+}
 
-    public class NbaTeams
-    {
-        public NbaTeam Home { get; set; }
-        public NbaTeam Visitors { get; set; }
-    }
+public class NbaStatus
+{
+    public string Short { get; set; }
+    public string Long { get; set; }
+    public string Clock { get; set; }
+    public bool Halftime { get; set; }
+}
 
-    public class NbaTeam
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Nickname { get; set; }
-        public string Code { get; set; }   
-        public string Logo { get; set; }   
-    }
+public class NbaTeams
+{
+    public NbaTeam Home { get; set; }
+    public NbaTeam Visitors { get; set; }
+}
 
-    public class NbaScores
-    {
-        public NbaScoreDetails Home { get; set; }
-        public NbaScoreDetails Visitors { get; set; }
-    }
+public class NbaTeam
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Nickname { get; set; }
+    public string Code { get; set; }
+    public string Logo { get; set; }
+}
 
-    public class NbaScoreDetails
-    {
-        public int? Quarter1 { get; set; } 
-        public int? Quarter2 { get; set; }
-        public int? Quarter3 { get; set; }
-        public int? Quarter4 { get; set; }
-        public int? Overtime { get; set; } 
-        public int? Total { get; set; }    
-    }
+public class NbaScores
+{
+    public NbaScoreDetails Home { get; set; }
+    public NbaScoreDetails Visitors { get; set; }
+}
 
+public class NbaScoreDetails
+{
+    public int? Win { get; set; }
+    public int? Loss { get; set; }
+    public int? Points { get; set; }                   // Total points scored by the team
+    public List<string> Linescore { get; set; }        // Quarter-by-quarter scores as a list of strings
+    public SeriesRecord Series { get; set; }           // Series win-loss record
+}
 
+public class SeriesRecord
+{
+    public int? Win { get; set; }
+    public int? Loss { get; set; }
+}
