09a4ba8ec6742a87ef2b6ec834c91b592937ad84
David Waiganjo <dwaiganjo@seattleu.edu>
2024-11-12 16:43:05 -0800

    Update the EPLMatchesModel.cs to allow for different data naming.

--- src/Models/EPLMatchesModel.cs
+++ src/Models/EPLMatchesModel.cs
@@ -4,9 +4,16 @@ using System.Collections.Generic;
 
 public class FixtureResponse
 {
-    public LeagueInfo League { get; set; } 
+    [JsonProperty("league")]
+    public LeagueInfo League { get; set; }
+
+    [JsonProperty("season")]
     public int Season { get; set; }
+
+    [JsonProperty("teams")]
     public FixtureTeams Teams { get; set; }
+
+    [JsonProperty("score")]
     public FixtureScores Scores { get; set; }
 
     [JsonProperty("date")]
@@ -14,62 +21,110 @@ public class FixtureResponse
 
     [JsonProperty("status")]
     public FixtureStatus Status { get; set; }
+
+    [JsonProperty("stage")]
     public string Stage { get; set; }
 }
 
-public class LeagueInfo 
+public class LeagueInfo
 {
+    [JsonProperty("id")]
     public int Id { get; set; }
+
+    [JsonProperty("name")]
     public string Name { get; set; }
+
+    [JsonProperty("country")]
     public string Country { get; set; }
 }
 
 public class FixtureDate
 {
+    [JsonProperty("start")]
     public string Start { get; set; }
+
+    [JsonProperty("timezone")]
     public string Timezone { get; set; }
+
+    [JsonProperty("datetime")]
+
     public DateTimeOffset DateTime { get; set; }
+
+    [JsonProperty("timestamp")]
     public long Timestamp { get; set; }
 }
 
 public class FixtureStatus
 {
+    [JsonProperty("short")]
     public string Short { get; set; }
+
+    [JsonProperty("long")]
     public string Long { get; set; }
+
+    [JsonProperty("clock")]
     public string Clock { get; set; }
+
+    [JsonProperty("halftime")]
     public bool Halftime { get; set; }
 }
 
 public class FixtureTeams
 {
+    [JsonProperty("home")]
     public TeamE Home { get; set; }
+
+    [JsonProperty("away")]
     public TeamE Visitors { get; set; }
 }
 
 public class TeamE
 {
+    [JsonProperty("id")]
     public int Id { get; set; }
+
+    [JsonProperty("name")]
     public string Name { get; set; }
+
+    [JsonProperty("city")]
     public string City { get; set; }
+
+    [JsonProperty("code")]
     public string Code { get; set; }
+
+    [JsonProperty("logo")]
     public string Logo { get; set; }
 }
 
 public class FixtureScores
 {
-    public ScoreDetails1 Home { get; set; }
-    public ScoreDetails1 Visitors { get; set; }
+    [JsonProperty("halftime")]
+    public ScoreDetails1 Halftime { get; set; }
+
+    [JsonProperty("fulltime")]
+    public ScoreDetails1 Fulltime { get; set; }
+
+    [JsonProperty("extratime")]
+    public ScoreDetails1 ExtraTime { get; set; }
+
+    [JsonProperty("penalty")]
+    public ScoreDetails1 Penalty { get; set; }
 }
 
 public class ScoreDetails1
 {
-    public int? Points { get; set; }                   // Total points scored by the team
-    public List<string> Linescore { get; set; }        // Period-by-period scores as a list of strings
-    public SeriesRecord1 Series { get; set; }          // Series win-loss record, if applicable
+    [JsonProperty("home")]
+    public int? Home { get; set; }
+
+    [JsonProperty("away")]
+    public int? Away { get; set; }
 }
 
 public class SeriesRecord1
 {
+    [JsonProperty("win")]
     public int? Win { get; set; }
+
+    [JsonProperty("loss")]
     public int? Loss { get; set; }
 }
