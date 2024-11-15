42928ab5c4d92140f1b58010d43a1a86c049e7f6
Mann-1702 <mannshah1702@gmail.com>
2024-11-15 00:22:10 -0800

    Updated Index.cshtml.cs file located in src\Pages\Product folder for designing a search bar from where the user can quickly search for a team.

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,23 +1,20 @@
 using System.Collections.Generic;
-
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
-
     /// <summary>
     /// Mann Shah
     /// Hongye Xiong
-    /// David Waiganjo k
+    /// David Waiganjo K
     /// Ian Tjok
     /// </summary>
     public class IndexModel : PageModel
     {
-
         /// <summary>
         /// Default Constructor
         /// </summary>
@@ -33,15 +30,28 @@ namespace ContosoCrafts.WebSite.Pages.Product
         // Collection of the Data
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        // Property to capture the search term
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
         /// <summary>
         /// REST OnGet
-        /// Return all the data
+        /// Return all data or filter based on SearchTerm
         /// </summary>
         public void OnGet()
         {
-            Products = ProductService.GetAllData();
+            var allProducts = ProductService.GetAllData();
+
+            // Filter products if SearchTerm is provided
+            if (!string.IsNullOrEmpty(SearchTerm))
+            {
+                Products = allProducts.Where(p =>
+                    p.Title.Contains(SearchTerm, System.StringComparison.OrdinalIgnoreCase));
+            }
+            else
+            {
+                Products = allProducts;
+            }
         }
-
     }
-
 }
