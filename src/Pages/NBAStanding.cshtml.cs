0d6518850ae0fa7a0f57c0186d2d95cbf2b8cd10
David Waiganjo <dwaiganjo@seattleu.edu>
2024-11-29 13:41:44 -0800

    Added NBAStanding.cshtml.cs to allow getting appropriate data and proper visualization

---
+++ src/Pages/NBAStanding.cshtml.cs
@@ -0,0 +1,64 @@
+using ContosoCrafts.WebSite.Pages.Matches;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class NBAStandingsModel : PageModel
+    {
+        private readonly SportsApiClient _sportsApiClient;
+        private readonly ILogger<NBAMatchesModel> _logger;
+
+        public NBAStandingsModel(SportsApiClient sportsApiClient, ILogger<NBAMatchesModel> logger)
+        {
+            _sportsApiClient = sportsApiClient;
+            _logger = logger;
+        }
+
+        // List of NBA Games
+        public List<NBAStanding> Games { get; private set; }
+
+        // Year of the NBA Season (Default = 2024)
+        public int SeasonYear { get; private set; }
+
+        public IActionResult OnGet(string teamName = null, int year = 2024)
+        {
+
+            SeasonYear = year;
+
+            string leagueId = "standard";
+            string baseUrl = "https://v2.nba.api-sports.io";
+            string baseHost = "v2.aenba.api-sports.io";
+            string endPoint = "standings";
+
+            try
+            {
+                // Fetch game data for NFL season with specified year
+                Games = _sportsApiClient.GetGamesForSeason<NBAStanding>(leagueId, SeasonYear, baseUrl, baseHost, endPoint).ToList();
+
+                // Return page if no specified team to filter for
+                if (teamName == null)
+                {
+                    return Page();
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error fetching game results.");
+
+                // Handle error 
+                Games = new List<NBAStanding>();
+            }
+
+            return Page();
+        }
+    }
+}
