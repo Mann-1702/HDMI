f46f08e38884224cd522c82b067e3dfd64e1d4d0
Ian Tjok <ianptjok@hotmail.com>
2024-12-01 21:41:30 -0800

    Updated error page to take in error code/message in Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
@@ -13,15 +14,33 @@ namespace ContosoCrafts.WebSite.Pages
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         private readonly ILogger<ErrorModel> _logger;
+        public string ErrorMessage { get; set; }
 
+        public int ErrorCode { get; set; }
+
+        /// <summary>
+        /// Instantiates a logger to record error data
+        /// </summary>
+        /// <param name="logger"></param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet(int? statusCode = null)
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+            // Default message for unexpected errors
+            ErrorMessage = "An unexpected error occurred.";
+
+            ErrorCode = statusCode.GetValueOrDefault();
+
+            // Handle specific status codes
+            if (statusCode.HasValue)
+            {
+                ErrorMessage = ErrorCodeHandler.GetErrorMessage(ErrorCode);
+            }
         }
 
     }
