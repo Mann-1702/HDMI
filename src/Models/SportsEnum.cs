28a2168e71c3539d81bb423eb9247542e6959030
1HongyeXiong <hongye.xiong@outlook.com>
2024-12-02 15:08:15 -0800

    Created the int enum for the type of sports in the SportsEnum.cs

---
+++ src/Models/SportsEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class SportsEnum : ContentPage
+    {
+        public SportsEnum()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
